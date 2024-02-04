58de456fb919a9e4cbc14d4ae54c279f35f39b00
zypher420 <101986259+p1mo@users.noreply.github.com>
2024-02-04 17:05:11 +0100

    update to beta

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tauri-sycamore
-Tauri v2 test app using Sycamore
+
+#### Tauri v2 test app using Sycamore
+
+<a href="https://p1mo.github.io/tauri-wasm"><img src="https://img.shields.io/badge/tauri--wasm-v2_bindings-FE7A36"></a>
+
+---
+
+- [ ] `p1mo/tauri-wasm` -- __testing__
+
+---
+
+**Other**
+
+- [ ] updater (setup, server/static)
+- [ ] actions (pre-builds, releases, checksums, update_server, update_static)
--- system/src/main.rs
+++ system/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Window;
+use tauri::WebviewWindow;
 use tauri::Manager;
 use tauri::PhysicalPosition;
 
@@ -31,10 +31,28 @@ fn main() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
+        //.plugin(tauri_plugin_barcode_scanner::init())
+        //.plugin(tauri_plugin_biometric::init())
+        //.plugin(tauri_plugin_authenticator::init())
+        //.plugin(tauri_plugin_cli::init())
+        //.plugin(tauri_plugin_clipboard_manager::init())
+        //.plugin(tauri_plugin_deep_link::init())
+        //.plugin(tauri_plugin_dialog::init())
+        //.plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_log::Builder::default().build())
+        //.plugin(tauri_plugin_nfc::init())
+        //.plugin(tauri_plugin_notification::init())
+        //.plugin(tauri_plugin_positioner::init())
+        .plugin(tauri_plugin_process::init())
+        //.plugin(tauri_plugin_updater::Builder::default().build())
+        //.plugin(tauri_plugin_upload::init())
+        //.plugin(tauri_plugin_websocket::init())
+        //.plugin(tauri_plugin_window_state::Builder::default().build())
+        //.plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
 
-            let window = app.get_window("main").unwrap();
+            let window = app.get_webview_window("main").unwrap();
 
             #[cfg(debug_assertions)]{
                 set_monitor_2_center_top(&window);
@@ -102,8 +120,8 @@ fn main() {
         .expect("error while running tauri application");
 }
 
-
-fn set_monitor_2_center_top(window : &Window) {
+#[cfg(debug_assertions)]
+fn set_monitor_2_center_top(window : &WebviewWindow) {
     if let Ok(monitors) = window.available_monitors() {
         window.set_position(*monitors[1].position()).unwrap();
         window.center().unwrap();
